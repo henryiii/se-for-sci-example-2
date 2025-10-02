747bb615a375a747df2972fdc7792a22a261910f
Henry Schreiner <henryschreineriii@gmail.com>
2025-10-02 15:18:23 -0400

    tests: add a test suite

--- src/se_for_sci_example_2/__init__.py
+++ src/se_for_sci_example_2/__init__.py
@@ -1,2 +1,19 @@
+import dataclasses
+
+@dataclasses.dataclass(frozen=True)
+class Vector:
+    x: float
+    y: float
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def mag(self):
+        return (self.x**2 + self.y**2) ** 0.5
+
+
 def main() -> None:
     print("Hello from se-for-sci-example-2!")
---
+++ tests/test_simple.py
@@ -0,0 +1,24 @@
+from se_for_sci_example_2 import Vector
+
+import pytest
+
+
+def test_vector_ops():
+    v1 = Vector(1, 2)
+    v2 = Vector(3, 4)
+    assert v1 + v2 == Vector(4, 6)
+    assert v1 - v2 == Vector(-2, -2)
+    assert v1.mag() == pytest.approx(2.23606797749979)
+    assert v2.mag() == pytest.approx(5.0)
+
+
+def test_vector_repr():
+    v = Vector(1, 2)
+    assert repr(v) == "Vector(x=1, y=2)"
+
+
+def test_vector_eq():
+    v1 = Vector(1, 2)
+    v2 = Vector(1, 2)
+    assert v1 == v2
+
